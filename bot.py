39608b171946a83333de94d7e00ff5d23a8491c4
Duhyeon <korjack@duhyeon.kim>
2025-04-23 20:51:38 +0900

    feat: 아무도 없다면 bot 퇴장

--- bot.py
+++ bot.py
@@ -9,6 +9,18 @@ from config import BOT_TOKEN, DEFAULT_VOLUME
 
 client = BotClient()
 
+@client.event
+async def on_voice_state_update(member, before, after):
+    voice_client = discord.utils.get(client.voice_clients, guild=member.guild)
+
+    if voice_client and voice_client.channel:
+        if len(voice_client.channel.members) == 1:  # 봇만 남았다면
+            guild_name = voice_client.guild.name
+            channel_name = voice_client.channel.name
+            await voice_client.disconnect()
+            print(f"[{guild_name}] 서버의 음성채널 '{channel_name}'에서 유저가 없어 연결을 종료했습니다.")
+
+
 @client.tree.command(name="youtube", description="YouTube 영상 검색")
 @app_commands.describe(query="검색어", volume=f"음량 (0.0 ~ 100.0) / 기본값 {DEFAULT_VOLUME}")
 async def youtube(interaction: discord.Interaction, query: str, volume: float = DEFAULT_VOLUME):
